826f8b36e4f699778a401f01eaa003799d37f221
unknown <Skaiweltrading@gmail.com>
2024-09-30 15:03:34 +0800

    part3

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 def home_view(request):
-    return render(request, 'posts/home.html')
+    posts = Post.objects.all()
+    return render(request, 'posts/home.html', {'posts': posts})
+
+def post_create_view(request):
+    form = PostCreateForm()
+
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'posts/post_create.html', {'form': form})

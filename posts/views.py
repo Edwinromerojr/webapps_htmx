f445e16c0d98bc7de44f2ed69c0419bf3f56bad7
unknown <Skaiweltrading@gmail.com>
2024-10-01 08:47:48 +0800

    part4 webcrawler

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from bs4 import BeautifulSoup
+import requests
+from django.contrib import messages
 from .models import *
 from .forms import *
 
@@ -12,7 +15,24 @@ def post_create_view(request):
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+
+            website = requests.get(form.data['url'])
+            sourcecode = BeautifulSoup(website.text, 'html.parser')
+
+            find_image = sourcecode.select('meta[content^="https://live.staticflickr.com/"]')
+            image = find_image[0]['content']
+            post.image = image
+
+            find_title = sourcecode.select('h1.photo-title')
+            title = find_title[0].text.strip()
+            post.title = title
+
+            find_artist = sourcecode.select('a.owner-name')
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+
+            post.save()
             return redirect('home')
 
     return render(request, 'posts/post_create.html', {'form': form})
